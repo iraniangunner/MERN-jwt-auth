6257508d4596a8a11f7aff2c96ffe05594753157
fateh moonesi <f.moonesi@jsk.local>
2024-09-17 17:18:49 +0330

    some config added

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,13 +1,33 @@
+import "dotenv/config";
 import express from "express";
+import cors from "cors";
+import connectToDatabase from "./config/db";
+import { APP_ORIGIN, NODE_ENV, PORT } from "./constants/env";
+import cookieParser from "cookie-parser";
+import errorHandler from "./middleware/errorHandler";
+import catchErrors from "./utils/catchErrors";
+import { OK } from "./constants/http";
 
 const app = express();
 
-app.get("/", (req, res) => {
-  return res.status(200).json({
-    status: "healthy",
-  });
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors({ origin: APP_ORIGIN, credentials: true }));
+app.use(cookieParser());
+
+app.get("/", (req, res, next) => {
+  try {
+    return res.status(OK).json({
+      status: "healthy",
+    });
+  } catch (error) {
+    next(error);
+  }
 });
 
-app.listen(4004, () => {
-  console.log(`Server is running on port 4004 in development environment.`);
+app.use(errorHandler);
+
+app.listen(PORT, async () => {
+  console.log(`Server is running on port ${PORT} in ${NODE_ENV} environment.`);
+  await connectToDatabase();
 });
