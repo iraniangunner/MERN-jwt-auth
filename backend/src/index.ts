0fddd20b125c76407a09dc5c62eba26c06d399f9
fateh moonesi <f.moonesi@jsk.local>
2024-10-06 17:37:34 +0330

    backend completed and frontend initialized

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -7,6 +7,9 @@ import cookieParser from "cookie-parser";
 import errorHandler from "./middleware/errorHandler";
 import { OK } from "./constants/http";
 import authRoutes from "./routes/auth.route";
+import authenticate from "./middleware/authenticate";
+import userRoutes from "./routes/user.route";
+import sessionRoutes from "./routes/session.route";
 
 const app = express();
 
@@ -25,8 +28,14 @@ app.get("/", (req, res, next) => {
   }
 });
 
+//auth routes
 app.use("/auth", authRoutes);
 
+//protected routes
+app.use("/user", authenticate, userRoutes);
+app.use("/sessions", authenticate, sessionRoutes);
+
+//error handler
 app.use(errorHandler);
 
 app.listen(PORT, async () => {
